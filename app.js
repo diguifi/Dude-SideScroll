57a1e8b4377c075680220cfb83da96928fb1e883
diguifi <lumia-diguifi@hotmail.com>
2018-08-13 22:31:26 -0300

    Phaser setup

---
+++ app.js
@@ -0,0 +1,18 @@
+/// <reference path="build/phaser.d.ts" />
+var SimpleGame = /** @class */ (function () {
+    function SimpleGame() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+    SimpleGame.prototype.preload = function () {
+        this.game.load.image('logo', 'dude.png');
+    };
+    SimpleGame.prototype.create = function () {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    };
+    return SimpleGame;
+}());
+window.onload = function () {
+    var game = new SimpleGame();
+};
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,26 @@
+﻿/// <reference path="build/phaser.d.ts" />
+
+class SimpleGame {
+
+    constructor() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+
+    game: Phaser.Game;
+
+    preload() {
+        this.game.load.image('logo', 'dude.png');
+    }
+
+    create() {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    }
+
+}
+
+window.onload = () => {
+
+    var game = new SimpleGame();
+
+};
