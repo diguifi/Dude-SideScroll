fee853d0011e86887d2c181e600987af2a002792
diguifi <lumia-diguifi@hotmail.com>
2018-08-14 19:31:00 -0300

    Fix csproj, index and style

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,18 @@
-﻿/// <reference path="../engine/phaser.d.ts" />
-
-class SimpleGame {
-
-    constructor() {
+/// <reference path="../engine/phaser.d.ts" />
+var SimpleGame = /** @class */ (function () {
+    function SimpleGame() {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
     }
-
-    game: Phaser.Game;
-
-    preload() {
+    SimpleGame.prototype.preload = function () {
         this.game.load.image('logo', '../assets/images/dude.png');
-    }
-
-    create() {
+    };
+    SimpleGame.prototype.create = function () {
         var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
         logo.anchor.setTo(0.5, 0.5);
-    }
-
-}
-
-window.onload = () => {
-
+    };
+    return SimpleGame;
+}());
+window.onload = function () {
     var game = new SimpleGame();
-
 };
+//# sourceMappingURL=app.js.map
