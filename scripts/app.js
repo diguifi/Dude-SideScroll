4717788cfe58812e5ee64f1fc907d2556636d524
diguifi <lumia-diguifi@hotmail.com>
2018-08-14 20:16:01 -0300

    First interaction

--- scripts/app.js
+++ scripts/app.js
@@ -9,6 +9,8 @@ var SimpleGame = /** @class */ (function () {
     SimpleGame.prototype.create = function () {
         var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
         logo.anchor.setTo(0.5, 0.5);
+        logo.scale.setTo(0.2, 0.2);
+        this.game.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
     };
     return SimpleGame;
 }());
