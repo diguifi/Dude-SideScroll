08693ec31db2f52ca7deee7b963cee600d987a1d
diguifi <lumia-diguifi@hotmail.com>
2018-12-01 20:29:44 -0200

    First version of the cutscene 1

--- scripts/cutscenes/Cutscene1.ts
+++ scripts/cutscenes/Cutscene1.ts
@@ -10,6 +10,50 @@ export class Cutscene1 extends Phaser.State {
     levelManager: LevelManager;
     levelBase: LevelBase;
     skip: boolean = false;
+    dudeSprite: Phaser.Sprite;
+    index: number = 0;
+    narratorPhrase: string = '';
+    narratorLines: string[] = [
+        " ",
+        "This is Dude",
+        " ",
+        "he has the most generic name ever",
+        " ",
+        "yet he is a very happy dude",
+        "he lives in a land ruled by a king",
+        "and his daughter, the princess... Princess.",
+        " ",
+        "Yes, the name of the princess is Princess",
+        " ",
+        "One day...",
+        " ",
+        "Yes she was kidnapped",
+        "By an evil goblin!",
+        "Immeditially, Dude presented himself to rescue her",
+        "and save the kingdom once and for all!",
+        " ",
+    ];
+    dudePhrase: string = '';
+    dudeLines: string[] = [
+        " ",
+        " ",
+        "sup",
+        " ",
+        "Thanks",
+        " ",
+        " ",
+        " ",
+        "wtf, her name is 'Princess'?!",
+        " ",
+        "jesus, you suck at names",
+        " ",
+        "Let me guess, she was kidnapped",
+        " ",
+        " ",
+        " ",
+        " ",
+        "Let's do this!",
+    ];
 
     init(soundManager) {
         this.soundManager = soundManager;
@@ -25,6 +69,47 @@ export class Cutscene1 extends Phaser.State {
         this.levelManager.createParallax(500);
         this.game.world.bringToTop(this.levelManager.level.back);
         this.game.world.bringToTop(this.levelManager.level.walls);
+
+        this.dudeSprite = this.game.add.sprite(230, 250, 'dude');
+        this.dudeSprite.scale.setTo(2, 2);
+        this.dudeSprite.visible = false;
+
+        this.nextLine();
+    }
+
+    updateLine() {
+        this.dudeSprite.frame = 0;
+
+        this.narratorPhrase = this.narratorLines[this.index];
+        this.dudePhrase = this.dudeLines[this.index];
+
+        if (this.index == 2){
+            this.soundManager.jump.play();
+            this.dudeSprite.visible = true;
+        }
+        if (this.index == 4 || this.index == 8){
+            this.dudeSprite.frame = 4;
+        }
+        if (this.index == 10){
+            this.soundManager.damage.play();
+            this.dudeSprite.frame = 6;
+        }
+        
+        this.game.time.events.add(Phaser.Timer.SECOND * 2.2, this.nextLine, this);
+    
+    }
+
+    nextLine() {
+        this.game.time.events.removeAll();
+
+        this.index++;
+    
+        if (this.index < this.narratorLines.length){
+            this.game.time.events.repeat(80, this.narratorLines[this.index].length + 1, this.updateLine, this);
+        }
+        else{
+            this.fadeOut();
+        }
     }
 
     fadeOut() {
@@ -33,9 +118,9 @@ export class Cutscene1 extends Phaser.State {
     }
 
     render(){
-        this.game.debug.text("This is the base of the 1st cutscene's version", 220, 150);
-        this.game.debug.text("Soon there will be some storytelling here", 220, 200);
-        this.game.debug.text("Click to continue", 300, 250);
+        this.game.debug.text(this.narratorPhrase, 290, 370);
+        this.game.debug.text(this.dudePhrase, 300, 270);
+        this.game.debug.text("Click to skip", 350, 20);
     }
 
     startGame() {
